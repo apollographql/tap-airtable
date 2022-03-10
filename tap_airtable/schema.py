21afdd52bce5d1bc6414997ccd04215fc0de38d3
kellymondor <kellymondor@gmail.com>
2022-03-10 09:16:21 -0700

    Update schema.py
    Comment out exception

--- tap_airtable/schema.py
+++ tap_airtable/schema.py
@@ -69,7 +69,7 @@ def get_property_schema(field):
         property_schema = get_property_schema(field.get("options").get("result"))
     else:
         property_schema["type"] = ["null", "string"]
-        raise Exception(f"Found unsupported type: {airtable_type}.")
+#         raise Exception(f"Found unsupported type: {airtable_type}.")
 
     return property_schema
 
